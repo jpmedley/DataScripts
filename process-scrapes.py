c742f7d1671caf6ea18784742ef810e7ffece036
Joe Medley <jmedley@chromium.org>
2017-02-10 19:21:54 -0800

    Troubleshoot request.

--- process-scrapes.py
+++ process-scrapes.py
@@ -4,39 +4,44 @@ import json
 import os
 import re
 import string
+import sys
 
 OUT_PATH = "out/"
 
-cities = dict() #key = city name, value = years
+def processScrapes(startYear, endYear):
+  cities = dict() #key = city name, value = years
 
-listing = os.listdir(OUT_PATH)
-for file in listing:
-  if (string.find(file, ".html") > 0):
-    currentFile = open(file, 'r')
-    file = file.rpartition('.')[0]
-    year = file[-4:]
-    city = file[:(len(file) - 4)]
-    # print ("Reading %s for %s" % (city, year))
-    if (city not in cities):
-      cities[city] = dict()
-    for line in currentFile:
-      if (string.find(line, "rate vcrime2 murd2") > 0):
-        value = re.search('<font size="2">(.*)</font>', line)
-        cities[city][year] = value.group(1).strip()
-        break
+  listing = os.listdir(OUT_PATH)
+  for file in listing:
+    if (string.find(file, ".html") > 0):
+      currentFile = open((OUT_PATH + file), 'r')
+      file = file.rpartition('.')[0]
+      year = file[-4:]
+      city = file[:(len(file) - 4)]
+      # print ("Reading %s for %s" % (city, year))
+      if (city not in cities):
+        cities[city] = dict()
+      for line in currentFile:
+        if (string.find(line, "rate vcrime2 murd2") > 0):
+          value = re.search('<font size="2">(.*)</font>', line)
+          cities[city][year] = value.group(1).strip()
+          break
 
-murders = open('murders.csv', 'a')
-for city in cities:
-  print ("Creating and saving row for %s" % city)
-  row = city + "\t"
-  year = 1985
-  while (year < 2014):
-    if (str(year) in cities[city]):
-      row = row + cities[city][str(year)] + "\t"
-    else:
-      row = row + "\t"
-    year += 1
-  row = row + "\n"
-  murders.write(row)
-murders.close()
-print "Completed processing crime data."
+  murders = open(OUT_PATH + 'murders.csv', 'a')
+  for city in cities:
+    print ("Creating and saving row for %s" % city)
+    row = city + "\t"
+    year = int(startYear)
+    end = int(endYear)
+    while (year < end):
+      if (str(year) in cities[city]):
+        row = row + cities[city][str(year)] + "\t"
+      else:
+        row = row + "\t"
+      year += 1
+    row = row + "\n"
+    murders.write(row)
+  murders.close()
+  print "Completed processing crime data."
+
+processScrapes(sys.argv[1], sys.argv[2])
--- scrape-data.py
+++ scrape-data.py
@@ -26,8 +26,10 @@ def Cities():
 
 def getResponse(year, stateId, crimeCrossId):
 
+  # StateId=1& BJSPopulationGroupId=& CrimeCrossId=102& DataType=1& YearStart=2014& NextPage=Get+Table
   params = urllib.urlencode({'StateId': stateId, 
-                             'BJSPopulationGroupId': VIOLENT_CRIME_RATES, 
+                             # 'BJSPopulationGroupId': VIOLENT_CRIME_RATES, 
+                             'BJSPopulationGroupId': '',
                              'CrimeCrossId': crimeCrossId, 
                              'DataType': 3,
                              'YearStart': year,
@@ -39,19 +41,23 @@ def getResponse(year, stateId, crimeCrossId):
              "Content-Length": "97",
              "Cache-Control": "max-age=0",
              "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-             "Origin": "http://www.ucrdatatool.gov",
+             "Origin": "https://www.ucrdatatool.gov",
              "User-Agent": "Mozilla/5.0 (X11; CrOS armv7l 6812.88.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.153 Safari/537.36",
              "Content-Type": "application/x-www-form-urlencoded",
-             "Referer": "http://www.ucrdatatool.gov/Search/Crime/Local/OneYearofDataStepTwo.cfm",
-             "Accept-Encoding": "gzip, deflate",
+             "Referer": "https://www.ucrdatatool.gov/Search/Crime/Local/OneYearofDataStepTwo.cfm",
+             "Accept-Encoding": "gzip, deflate, br",
              "Accept-Language": "en-US,en;q=0.8",
-             "Cookie": "topItem=1c; CFID=106361124; CFTOKEN=a10fcd62b8337ef1-12AD05B5-EC17-3180-BF29864F30881E06; _ga=GA1.2.762111162.1442274583"}
+             "Cookie": "fsr.s=%7B%22v2%22%3A-2%2C%22v1%22%3A1%2C%22cp%22%3A%7B%22cxreplayaws%22%3A%22true%22%7D%2C%22rid%22%3A%22d464cf3-84849867-6167-1436-77641%22%2C%22to%22%3A4%2C%22c%22%3A%22http%3A%2F%2Fwww.ucrdatatool.gov%2FSearch%2FCrime%2FLocal%2FTrendsInOneVarStepTwo.cfm%22%2C%22pv%22%3A87%2C%22lc%22%3A%7B%22d0%22%3A%7B%22v%22%3A87%2C%22s%22%3Atrue%7D%7D%2C%22cd%22%3A0%2C%22sd%22%3A0%2C%22l%22%3A%22en%22%2C%22i%22%3A1%2C%22t%22%3A1%7D; BIGipServerbjs_http_pool=940669706.20480.0000; CFID=143409549; CFTOKEN=e8bb1aa183483088-7C40B983-AE8D-264B-134F49227B887381; topItem=1c; _gat_UA-49393820-1=1; _ga=GA1.2.947731664.1442451395; acs.t=%7B%22_ckX%22%3A1494467930367%2C%22rid%22%3A%22d464c27-83930372-b8e9-69ca-436fe%22%2C%22cp%22%3A%7B%22url%22%3A%22https%3A%2F%2Fwww.ucrdatatool.gov%2FSearch%2FCrime%2FLocal%2FRunCrimeOneYearofData.cfm%22%2C%22terms%22%3A%22%22%2C%22browser%22%3A%22Chrome%2056%22%2C%22os%22%3A%22Windows%22%2C%22flash%22%3A%2224.0%22%2C%22hosted%22%3A%22true%22%2C%22referrer%22%3A%22%22%2C%22site%22%3A%22bjs-gov%22%2C%22trigger_version%22%3A%2219.0.35%22%2C%22pv%22%3A%227%22%2C%22locale%22%3A%22en%22%2C%22cxreplayaws%22%3A%22true%22%2C%22dn%22%3A%22default%22%7D%2C%22pl%22%3A1%2C%22pv%22%3A7%2C%22def%22%3A0%2C%22browsepv%22%3A7%2C%22rc%22%3A%22true%22%2C%22grft%22%3A1486698161412%2C%22mid%22%3A%22d464c27-83640095-c961-f441-9708c%22%2C%22rt%22%3Afalse%2C%22cncl%22%3Afalse%2C%22rpid%22%3A%22d464c27-83640095-fede-b129-09e9d%22%2C%22dn%22%3A%22default%22%2C%22i%22%3A%22a%22%2C%22rw%22%3A1494472689911%7D"}
 
-  conn = httplib.HTTPConnection("www.ucrdatatool.gov", timeout=10)
+  conn = httplib.HTTPConnection("https://www.ucrdatatool.gov", timeout=5)
   url = "/Search/Crime/Local/RunCrimeOneYearofData.cfm"
   conn.request("POST", url, params, headers)
   try:
     response = conn.getresponse()
+    headers = response.getheaders()
+    for h in headers:
+      print h
+    print response.status
   except:
     raise
   return response
@@ -79,6 +85,7 @@ def getCityData(startYear, endYear):
       data = response.read()
       fileName = OUT_PATH + city['City'] + str(year) + ".html" 
       dataFile = open(fileName, 'w')
+      print data
       dataFile.write(data)
       dataFile.close()
       year += 1
